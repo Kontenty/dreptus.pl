bc068eed3461d9a1e147e5c3017bd1eeaab18ef5
Konrad Gorski <konrad.gorski@idea.edu.pl>
2022-11-08 20:34:20 +0000

    style: style header links and refactor

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,41 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
+
 import logo from "public/image/logo.png";
 import css from "./Header.module.css";
 
 type Props = {};
 
+const links = [
+  { name: "Strona główna", link: "/" },
+  { name: "Aktualności", link: "/news" },
+  { name: "Trasy", link: "/trips" },
+  { name: "Uczestnicy", link: "/p" },
+  { name: "Odznaki", link: "/o" },
+  { name: "Karta startowa", link: "/k" },
+  { name: "Kontakt", link: "/contact" },
+];
+
 const Header = (props: Props) => {
+  const router = useRouter();
   return (
     <header className={css.header}>
       <a href="/">
         <Image src={logo} alt="logo" height={150} layout="fixed" />
       </a>
       <nav className="flex gap-5 ml-5 self-center">
-        <Link href="/">Strona główna</Link>
-        <Link href="/news">Aktualności</Link>
-        <Link href="/">Trasy</Link>
-        <Link href="/">Uczestnicy</Link>
-        <Link href="/">Odznaki</Link>
-        <Link href="/">Karta startowa</Link>
-        <Link href="/">Kontakt</Link>
+        {links.map((link, i) => (
+          <Link key={i} href={link.link}>
+            <a
+              className={
+                router.pathname == link.link ? css.activeLink : css.link
+              }
+            >
+              {link.name}
+            </a>
+          </Link>
+        ))}
       </nav>
     </header>
   );
