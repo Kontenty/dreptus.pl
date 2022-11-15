6f5ccf62528d9d6e2eea8e7a3ee996cfb2fc6e66
Konrad Gorski <konrad.gorski@idea.edu.pl>
2022-11-15 12:17:03 +0000

    feat: add locations list to map

---
+++ components/LocationsList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type Props = {
+  list: { name: string; count: number; slug: string }[];
+};
+
+const LocationsList = ({ list }: Props) => {
+  return (
+    <div className="flex flex-col gap-1">
+      {list.map((loc) => (
+        <div
+          key={loc.slug}
+          role="button"
+          className="bg-blue-700 text-white p-3 rounded cursor-pointer"
+        >
+          {loc.name} ({loc.count})
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LocationsList;
--- pages/trips/index.tsx
+++ pages/trips/index.tsx
@@ -1,15 +1,25 @@
-import Link from "next/link";
+// import Link from "next/link";
+import { GetStaticProps } from "next";
+import LocationsList from "components/LocationsList";
+import { getLocations } from "lib/db";
+// import Map from "components/map/at-gmap-api";
 
-export default function Trips() {
+export const getStaticProps: GetStaticProps = async () => {
+  const trips = await getLocations();
+  return {
+    props: { trips: trips ? JSON.parse(JSON.stringify(trips)) : [] }, // will be passed to the page component as props
+  };
+};
+
+interface Props {
+  trips: { name: string; count: number; slug: string }[];
+}
+
+export default function Trips({ trips }: Props) {
   return (
-    <div className="justify-self-center">
-      <h1 className="text-3xl mb-10">
-        <span className="text-5xl animate-pulse">⚒</span>
-        <span> - strona w trakcie budowy</span>
-      </h1>
-      <Link href="/">
-        <a className="underline underline-offset-4">Wróć do początku</a>
-      </Link>
+    <div className="flex gap-6 w-[1100px] mx-auto py-4">
+      <LocationsList list={trips} />
+      <div className="flex-1 flex-grow w-10 bg-red-400">1</div>
     </div>
   );
 }
