b9e93fd151d4284e2707d3c6498d5e7180304771
Konrad Gorski <konrad.gorski@idea.edu.pl>
2023-01-25 12:32:08 +0000

    fix(revalidate): use fetch instead redirect, add logging to axiom

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import dotenv from "dotenv";
+import { log } from "next-axiom";
 dotenv.config();
 const token = process.env.REVALIDATE_TOKEN;
 
@@ -11,7 +12,9 @@ export default async function handler(
     return res.status(401).json({ message: "Invalid token" });
   }
   if (req.headers.host?.includes("dreptus.vercel.app")) {
-    return res.redirect(307, `https://xn--dreptu-8ib.pl${req?.url}`);
+    const response = await fetch(`https://xn--dreptu-8ib.pl${req?.url}`);
+    const data = await response.json();
+    return res.json(data);
   }
 
   const slug = req.query?.slug;
@@ -45,6 +48,7 @@ export default async function handler(
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
+    log.error("Revalidation error", { error: err });
     return res.status(500).send("Error revalidating");
   }
 }
