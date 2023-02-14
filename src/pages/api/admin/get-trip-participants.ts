e0cb9c2341625b352a7d3e6f1212827a118dee03
Konrad Gorski <konrad.gorski@idea.edu.pl>
2023-02-14 13:26:02 +0000

    feat: add endpoint for participants on selected trip

---
+++ src/pages/api/admin/get-trip-participants.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prismaClient from "src/lib/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const id = Number(req.query.id);
+  if (typeof id === "number") {
+    try {
+      const data = await prismaClient.wp_posts.findUnique({
+        include: {
+          trip_participant: {
+            include: { participant: true },
+          },
+        },
+        where: { ID: id },
+      });
+      const tripParticipants = {
+        name: data?.post_title.replace("<br>", " ").replaceAll("  ", ""),
+        participants: data?.trip_participant.map((tp) => ({
+          ...tp,
+          name: tp.participant.name,
+          origin: tp.participant.origin,
+        })),
+      };
+
+      return res.status(200).send(tripParticipants);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  }
+  return res.status(404);
+}
