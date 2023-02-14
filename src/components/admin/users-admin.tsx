65cf805e75d62968b375bb7c6ccc356ecf68e48f
Konrad Gorski <konrad.gorski@idea.edu.pl>
2023-02-14 13:27:00 +0000

    feat: show table of trip participants in users admin

--- src/components/admin/users-admin.tsx
+++ src/components/admin/users-admin.tsx
@@ -1,7 +1,30 @@
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
 import React from "react";
+import useSWR from "swr";
 
 const UsersAdmin = () => {
-  return <div>UsersAdmin</div>;
+  const { data } = useSWR("/api/admin/get-trip-participants?id=11870");
+  return (
+    <div>
+      <DataTable value={data?.participants}>
+        <Column field="name" header="Imię i Nazwisko" />
+        <Column field="origin" header="Klub / miejscowość" />
+        <Column field="answers" header="Odpowiedzi" />
+        <Column
+          body={(row) =>
+            new Intl.DateTimeFormat("pl-PL", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            }).format(new Date(row.report_date))
+          }
+          field="report_date"
+          header="Data zgłoszenia"
+        />
+      </DataTable>
+    </div>
+  );
 };
 
 export default UsersAdmin;
