56f295f36f4d603995e4f3be312e265951d5e8f5
Konrad Gorski <konradgrsk@gmail.com>
2024-11-14 12:53:34 +0100

    chore: eslint 9, remove duplicate configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dreptuś.pl",
+      },
+      {
+        protocol: "https",
+        hostname: "xn--dreptu-8ib.pl",
+      },
+      {
+        protocol: "https",
+        hostname: "wp.xn--dreptu-8ib.pl",
+      },
+      {
+        protocol: "http",
+        hostname: "dreptus.cal24.pl",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/uczestnicy-tras",
+        destination: "/participants",
+        permanent: false,
+      },
+      {
+        source: "/lista-tras",
+        destination: "/trips",
+        permanent: false,
+      },
+      {
+        source: "/karta-startowa",
+        destination: "/form",
+        permanent: false,
+      },
+      {
+        source: "/aktualnosci",
+        destination: "/news",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   plugins: {
     tailwindcss: {},
   },
