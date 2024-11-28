b93faf802c6a76be900f803857c5d410aaf70de4
Konrad Gorski <konrad_gorski@docupace.com>
2024-11-28 22:05:16 +0100

    own handlers in graphql api

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -2,9 +2,9 @@
 import { createYogaContext } from "@/lib/graphql/context";
 import { schema } from "@/lib/graphql/schema";
 import { createYoga } from "graphql-yoga";
+import { NextResponse } from "next/server";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const { handleRequest } = createYoga<any>({
+const { handleRequest } = createYoga({
   schema,
 
   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
@@ -18,8 +18,28 @@ const { handleRequest } = createYoga<any>({
 
 // export const GET = handleRequest;
 
-export {
-  handleRequest as GET,
-  handleRequest as POST,
-  handleRequest as OPTIONS,
-};
+// export {
+//   handleRequest as GET,
+//   handleRequest as POST,
+//   handleRequest as OPTIONS,
+// };
+export async function GET(req: Request) {
+  // Define a context object if needed or use an empty object.
+  const ctx = {};
+
+  // Forward the request to the GraphQL handler with both request and context.
+  return handleRequest(req, ctx);
+}
+
+export async function POST(req: Request) {
+  // Define a context object if needed or use an empty object.
+  const ctx = {};
+
+  // Forward the request to the GraphQL handler with both request and context
+  return handleRequest(req, ctx);
+}
+
+export async function OPTIONS() {
+  // Handle OPTIONS requests here if needed, usually for CORS.
+  return NextResponse.json({ message: "OPTIONS request received" });
+}
