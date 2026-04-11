60d00f340a449bd7c84f531dd114817463d8d4e5
Konard Górski <konradgrsk@gmail.com>
2026-04-11 13:10:26 +0200

    fix: hide signup, remove old auth compoents

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,18 +1,9 @@
-import { LogInButton } from "@/components/auth/LogInButton";
 import Main from "@/components/ui/Main";
 import { getTrips } from "@/lib/db";
 import { sortTrips } from "@/lib/utils";
 import TripParticipantsManager from "./TripParticipantsManager";
 
 export default async function AdminPage() {
-  const session = true;
-  if (!session) {
-    return (
-      <div className="center-hv grow">
-        <LogInButton />
-      </div>
-    );
-  }
   // Fetch trips data in Server Component
   const tripsParticipantsData = await getTrips();
   const tripsParticipants = tripsParticipantsData
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,13 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <main className="flex justify-center items-center grow pb-10">
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            footerAction: { display: "none" },
+          },
+        }}
+      />
     </main>
   );
 }
